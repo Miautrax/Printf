af3cd00741ae89c8e56ff319e9daa378d9edb525
Miautrax <rvadri.9.12@gmail.com>
2025-03-17 16:51:24 +0100

    Add files via upload

--- ft_printf.c
+++ ft_printf.c
@@ -13,30 +13,89 @@
 #include <libft.h>
 #include <stdarg.h>
 #include <stddef.h>
+#include <unistd.h>
 
-put_format (va_list *args, const char c, size_t *size)
+int put_format(char spec, va_list args, int *count)
 {
-	if (c == )
+	char			c;
+	char			*str;
+	unsigned long	ptr;
+
+	if (spec == 'c')
+	{
+		c = va_arg(args, int);
+		write(1, &c, 1);
+		(*count)++;
+	}
+	else if (spec == 's')
+	{
+		str = va_arg(args, char *);
+		ft_putstr_cnt(str, count);
+	}
+	else if (spec == 'p')
+	{
+		ptr = (unsigned long)va_arg(args, void *);
+		write(1, "0x", 2);
+		*count += 2;
+		ft_puthex_cnt(ptr, count, "0123456789abcdef");
+	}
+	else
+		return (put_format2(spec, args, count));
+	return (*count);
+}
+
+int put_format2(char spec, va_list args, int *count)
+{
+    unsigned int u;
+    int n;
+
+	if (spec == 'd' || spec == 'i')
+	{
+		n = va_arg(args, int);
+		ft_putlong_cnt(n, count);
+	}
+	else if (spec == 'u')
+	{
+		u = va_arg(args, unsigned int);
+		ft_putuint_cnt(u, count);
+	}
+	else if (spec == 'x' || spec == 'X')
+	{
+		u = va_arg(args, unsigned int);
+		if (spec == 'x')
+			ft_puthex_cnt(u, count, "0123456789abcdef");
+		else
+			ft_puthex_cnt(u, count, "0123456789ABCDEF");
+	}
+	else if (spec == '%')
+	{
+		write(1, "%", 1);
+		(*count)++;
+	}
+	return (*count);
 }
 
 int	ft_printf(char const *format, ...)
 {
 	va_list	args;
-	size_t	i;
-	
+	int		count;
+
+	count = 0;
 	va_start(args, format);
-	while (format)
+	while (*format)
 	{
-		if (format[i] = '%')
+		if (*format == '%' && *(format + 1))
 		{
-			i++;
-			put_format(&args, format, &size);
+			format++;
+			put_format(*format, args, &count);
 		}
 		else
 		{
-			ft_putchar_fd(format[i])
-			size++;
+			write(1, format, 1);
+			count++;
 		}
-		i++;
+		format++;
 	}
+	va_end(args);
+	return count;
 }
